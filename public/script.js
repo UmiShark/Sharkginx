093816e5b47ec82fa119d99e60e58492640aadad
UmiShark <62315593+UmiShark@users.noreply.github.com>
2022-01-02 16:40:52 -0500

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,34 @@
+function go() {
+  var input = document.getElementById("url-target");
+  if (input.value) {
+    window.open("/main/" + input.value);
+  } else {
+    alert("Please provide a link. It does not require https://.");
+  }
+}
+
+const btn = document.querySelector(".checkbox");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.toggle("dark-theme");
+
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-theme");
+}
+
+btn.addEventListener("click", function () {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+    var theme = document.body.classList.contains("light-theme")
+      ? "light"
+      : "dark";
+  } else {
+    document.body.classList.toggle("dark-theme");
+    var theme = document.body.classList.contains("dark-theme")
+      ? "dark"
+      : "light";
+  }
+  localStorage.setItem("theme", theme);
+});
